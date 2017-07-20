92a578530afac163b69da5c75381304b3737aae1
xtwang <xtwang@thoughtworks.com>
2017-07-20 16:17:12 +0800

    add event listener to handle the click and input

---
+++ src/js/main.js
@@ -0,0 +1,44 @@
+require('../style/invitation.scss')
+
+var $users = document.getElementsByName('users')
+var $inviteButton = document.getElementById('invite')
+var $main = document.getElementsByTagName('main')[0]
+var $eMail = document.getElementById('e-mail')
+
+function refreshInviteButtonStatus () {
+  var $selectedUsers = document.querySelectorAll('input[name="users"]:checked');
+  var isDisableInvite = !($selectedUsers.length || $eMail.value)
+
+  $inviteButton.disabled = isDisableInvite;
+  $inviteButton.style.cursor = isDisableInvite ? 'not-allowed' : 'pointer';
+}
+
+function calcAvailableNumber () {
+  var $selectedUsers = document.querySelectorAll('input[name="users"]:checked');
+  var $availableNumberBox = document.getElementById('availableNumber')
+
+  $availableNumberBox.innerText = $users.length - $selectedUsers.length
+  refreshInviteButtonStatus()
+}
+
+$main.addEventListener('click', function(evt) {
+  if(evt.target.type !== 'checkbox') return
+
+  calcAvailableNumber()
+})
+
+$eMail.addEventListener('keyup', function() {
+  refreshInviteButtonStatus()
+})
+
+$inviteButton.addEventListener('click', function() {
+  var checkedValue = $eMail.value + ' ';
+
+  $users.forEach(function (item) {
+    if(item.checked) checkedValue += item.value + ' '
+  })
+
+  alert(checkedValue + 'has invited.')
+})
+
+calcAvailableNumber()
